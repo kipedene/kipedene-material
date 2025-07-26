bde6064ce3c0d517dca0df467337120fc9a562fe
Kipédène Coulibaly <kipedene.coulibaly@gmail.com>
2025-07-26 15:02:04 +0200

    Initial commit

---
+++ projects/material/src/lib/material.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'km-material',
+  imports: [],
+  template: `
+    <p>
+      material works!
+    </p>
+  `,
+  styles: ``
+})
+export class Material {
+
+}
